609f290f0d2a84000ac1634ac11779aad187a454
ema <mccoy39082@163.com>
2024-03-18 06:05:26 +0800

    Updated to net8.0

--- build/nsis_pack.bat
+++ build/nsis_pack.bat
@@ -1,5 +1,5 @@
-rd /s /q ..\src\GenshinWoodmen\bin\x64\Release\net6.0-windows10.0.18362.0\publish\win-x64\GenshinWoodmen
-mkdir ..\src\GenshinWoodmen\bin\x64\Release\net6.0-windows10.0.18362.0\publish\win-x64\GenshinWoodmen
-copy ..\src\GenshinWoodmen\bin\x64\Release\net6.0-windows10.0.18362.0\publish\win-x64\* ..\src\GenshinWoodmen\bin\x64\Release\net6.0-windows10.0.18362.0\publish\win-x64\GenshinWoodmen
-7z a -mx9 GenshinWoodmen.7z ..\src\GenshinWoodmen\bin\x64\Release\net6.0-windows10.0.18362.0\publish\win-x64\GenshinWoodmen
+rd /s /q ..\src\GenshinWoodmen\bin\x64\Release\net8.0-windows10.0.19041.0\publish\win-x64\GenshinWoodmen
+mkdir ..\src\GenshinWoodmen\bin\x64\Release\net8.0-windows10.0.19041.0\publish\win-x64\GenshinWoodmen
+copy ..\src\GenshinWoodmen\bin\x64\Release\net8.0-windows10.0.19041.0\publish\win-x64\* ..\src\GenshinWoodmen\bin\x64\Release\net8.0-windows10.0.19041.0\publish\win-x64\GenshinWoodmen
+7z a -mx9 GenshinWoodmen.7z ..\src\GenshinWoodmen\bin\x64\Release\net8.0-windows10.0.19041.0\publish\win-x64\GenshinWoodmen
 @pause
